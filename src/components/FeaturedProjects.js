f2763eb2ec1416bbfd52617ccadd4013450b9249
eric <ericmichaelsnell@gmail.com>
2018-10-04 22:02:56 -0400

    featured projects made responsive

--- src/components/FeaturedProjects.js
+++ src/components/FeaturedProjects.js
@@ -1,24 +1,28 @@
-import React, { Component } from 'react';
-import ProjectCard from './ProjectCard';
-import { Flex, Box } from '@rebass/grid';
-import { Heading, Caps } from 'rebass';
-import featured from '../featured';
+import React, { Component } from "react";
+import ProjectCard from "./ProjectCard";
+import { Flex, Box } from "@rebass/grid";
+import { Heading, Caps } from "rebass";
+import featured from "../featured";
 
 class FeaturedProjects extends Component {
   render(props) {
     console.log(featured);
     return (
-      <div style={{paddingBottom: 50}}>
-        <Heading style={{paddingBottom: 50}}><Caps>Featured Projects</Caps></Heading>
+      <div style={{ paddingBottom: 50 }}>
+        <Heading style={{ paddingBottom: 50 }}>
+          <Caps>Featured Projects</Caps>
+        </Heading>
         <div>
-          <Flex align='center'>
-          {
-            featured.map((project) => 
-              <Box width={1/3} px={2}>
+          <Flex
+            alignItems="center"
+            flexWrap="wrap"
+            justifyContent="space-around"
+          >
+            {featured.map(project => (
+              <Box px={2} py={20}>
                 <ProjectCard project={project} />
               </Box>
-            )
-          }
+            ))}
           </Flex>
         </div>
       </div>
